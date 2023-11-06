240a16b688b6389a7ece867a4a762ad17d5b7453
FaliRonda <rafael.ronda.garcia@gmail.com>
2023-11-06 13:06:20 +0100

    refactor: added team scenes and updated ignore

---
+++ Assets/Scripts/NarrativeDirector.cs
@@ -0,0 +1,15 @@
+using TMPro;
+using UnityEngine;
+
+public class NarrativeDirector : MonoBehaviour
+{
+    public GameObject narrativeCurtain;
+    public TMP_Text narrativeText;
+    
+    public DialogueSO[] narrativeDialogues;
+
+    public void ShowNextNarrative()
+    {
+        
+    }
+}
