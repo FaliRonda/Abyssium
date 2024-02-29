fc580ff1d1c8ca2924b201eb7fa8e010fd9d25d3
FaliRonda <rafael.ronda.garcia@gmail.com>
2024-02-29 13:57:50 +0100

    gd: improved gamepad vibration feeling

--- Assets/Scripts/Services/GamepadVibrationService.cs
+++ Assets/Scripts/Services/GamepadVibrationService.cs
@@ -5,16 +5,23 @@ using UnityEngine.InputSystem;
 public class GamepadVibrationService : IService
 {
     private Gamepad gamepad;
-
+    private float currentIntensity;
     public void Initialize() {
         gamepad = Gamepad.current;
     }
 
-    public void SetControllerVibration(float intensity, float duration) {
-        if (gamepad != null) {
-            gamepad.SetMotorSpeeds(intensity, intensity);
-            Sequence sequence = DOTween.Sequence();
-            sequence.AppendInterval(duration).AppendCallback(() => gamepad.SetMotorSpeeds(0, 0));
+    public void SetControllerVibration(float intensity, float duration)
+    {
+        currentIntensity = 0;
+        
+        if (gamepad != null)
+        {
+            DOTween.To(() => currentIntensity, x => currentIntensity = x, intensity, duration)
+                .OnUpdate(() => {
+                    gamepad.SetMotorSpeeds(currentIntensity, currentIntensity);
+                })
+                .SetEase(Ease.OutBack)
+                .OnComplete(() => { gamepad.SetMotorSpeeds(0, 0); });
         }
     }
 }
