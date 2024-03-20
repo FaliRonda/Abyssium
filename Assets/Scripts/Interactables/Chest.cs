c2a0e018914658f8544307c829de92de8e91cb3f
FaliRonda <rafael.ronda.garcia@gmail.com>
2024-03-20 13:38:41 +0100

    feat: chest prepared for puzzle implementation

---
+++ Assets/Scripts/Interactables/Chest.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Chest : Interactable
+{
+    public override void Interact(PJ pj)
+    {
+        Debug.Log("Chest Interacting");
+        // Activar la UI la primera vez
+
+        // Detectar las flechas para seleccionar los botones mientras esté activo
+
+        // Actualizar el valor del seleccionado
+
+        // Comprobar si la combinación actual es la correcta
+
+        // Cerrar la UI si la opción es la última
+    }
+}
